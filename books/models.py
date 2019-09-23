71d353e1973a083fa732ee86b8ee24ba70b70bf1
srikanthpragada <srikanthpragada@gmail.com>
2019-09-23 07:56:33 +0530

    23-SEP

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    publisher = models.CharField(max_length=10)
+    price = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.id},{self.title},{self.publisher},{self.price}"
